1234c3bc7c540c4802d742c18aa2d81db4314666
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-04-17 11:17:21 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -21,42 +21,6 @@ const userSchema = Schema(
       required: true,
       min: 6,
     },
-    profilePicture: {
-      type: String,
-      default: "",
-    },
-    coverPicture: {
-      type: String,
-      default: "",
-    },
-    followers: {
-      type: Array,
-      default: [],
-    },
-    followings: {
-      type: Array,
-      default: [],
-    },
-    isAdmin: {
-      type: Boolean,
-      default: false,
-    },
-    desc: {
-      type: String,
-      max: 50,
-    },
-    city: {
-      type: String,
-      max: 50,
-    },
-    from: {
-      type: String,
-      max: 50,
-    },
-    relationship: {
-      type: Number,
-      enum: [1, 2, 3],
-    },
     subscription: {
       type: String,
       enum: ["starter", "pro", "business"],
