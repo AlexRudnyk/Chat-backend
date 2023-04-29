58c4cc3d60e9bbcb08bc510d9d0e8cd3ace40ff6
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-04-29 16:43:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,50 +9,6 @@ const messageRoute = require("./routes/messages");
 
 const app = express();
 
-const io = require("socket.io")(4040, {
-  cors: "*",
-});
-
-let users = [];
-
-const addUser = (userId, socketId) => {
-  !users.some((user) => user.userId === userId) &&
-    users.push({ userId, socketId });
-};
-
-const removeUser = (socketId) => {
-  users = users.filter((user) => user.socketId !== socketId);
-};
-
-io.on("connection", (socket) => {
-  // when connect
-  console.log("new user connected");
-
-  // take userId and socketId from user
-  socket.on("addUser", (userId) => {
-    addUser(userId, socket.id);
-    io.emit("getUsers", users);
-  });
-
-  // send and get messages
-  socket.on("sendMessage", ({ senderId, senderName, text, date, time }) => {
-    io.emit("getMessage", {
-      senderId,
-      senderName,
-      text,
-      date,
-      time,
-    });
-  });
-
-  // when disconnect
-  socket.on("disconnect", () => {
-    console.log("a user disconnected!");
-    removeUser(socket.id);
-    io.emit("getUsers", users);
-  });
-});
-
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
